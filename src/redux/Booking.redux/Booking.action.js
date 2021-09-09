18256630b5c5497d15618eae496101c800383031
Will Ley <wley3337@gmail.com>
2021-09-09 10:36:22 -0400

    wip commit moving to other laptop

--- src/redux/Booking.redux/Booking.action.js
+++ src/redux/Booking.redux/Booking.action.js
@@ -6,163 +6,140 @@ export const COMMIT_CHANGE = 'COMMIT_CHANGE';
 export const UPDATING_CHECKBOX = 'UPDATING_CHECKBOX';
 export const DROPDOWN_CHANGE = 'DROPDOWN_CHANGE';
 export const SET_LIBRARIES_AND_LANGUAGES = 'SET_LIBRARIES_AND_LANGUAGES';
+export const SET_BOOKING_LANGUAGE = 'SET_BOOKING_LANGUAGE';
 export const SET_TIMES = 'SET_TIMES';
 
-const fakeLanguageData = {
-  data: ['English', 'Spanish', 'French', 'German']
-}
+// const fakeLanguageData = {
+//   data: ['English', 'Spanish', 'French', 'German'],
+// };
 
-const fakeLibraryData = {
-  data: ['fake library1', 'fake library2', 'fake library3', 'fake library4']
-}
+// const fakeLibraryData = {
+//   data: ['fake library1', 'fake library2', 'fake library3', 'fake library4'],
+// };
 
-// ISO time format
-const fakeTimeData = {
-    slot1: {
-      start_time: '2021-05-15T09:00:00.000-04:00',
-      end_time: '2021-05-15T10:00:23.000-04:00'
-    },
-    slot2: {
-      start_time: '2021-05-17T15:00:00.000-04:00',
-      end_time: '2021-05-17T16:00:00.000-04:00'
-    },
-    slot3: {
-      start_time: '2021-05-18T21:00:00.000-04:00',
-      end_time: '2021-05-18T22:00:00.000-04:00'
-    },
-    slot4: {
-      start_time: '2021-05-19T23:00:00.000-04:00',
-      end_time: '2021-05-19T24:00:00.000-04:00'
-    }
-}
+// // ISO time format
+// const fakeTimeData = {
+//   slot1: {
+//     start_time: '2021-05-15T09:00:00.000-04:00',
+//     end_time: '2021-05-15T10:00:23.000-04:00',
+//   },
+//   slot2: {
+//     start_time: '2021-05-17T15:00:00.000-04:00',
+//     end_time: '2021-05-17T16:00:00.000-04:00',
+//   },
+//   slot3: {
+//     start_time: '2021-05-18T21:00:00.000-04:00',
+//     end_time: '2021-05-18T22:00:00.000-04:00',
+//   },
+//   slot4: {
+//     start_time: '2021-05-19T23:00:00.000-04:00',
+//     end_time: '2021-05-19T24:00:00.000-04:00',
+//   },
+// };
 
+// export const mentorChange = () => {
+//   return { type: MENTOR_CHANGE };
+// };
 
-export const mentorChange = () => {
-  return {
-    type: MENTOR_CHANGE
-  }
-}
+// export const setBookingLanguage = (language) => {
+//   return { type: SET_BOOKING_LANGUAGE, payload: language };
+// };
+// export const commitChange = () => {
+//   return { type: COMMIT_CHANGE };
+// };
 
-export const commitChange = () => {
-  return {
-    type: COMMIT_CHANGE
-  }
-}
+// /**
+//  * Update Booking form, then fetch booking time slots if all options are
+//  * selected.
+//  */
+// export const updatingBookingForm =
+//   (optionName, optionValue) => async (dispatch, getState) => {
+//     dispatch(updateFormValues(optionName, optionValue));
+//     const { language, library, time_zone, weekday } = getState().booking;
+//     if ((language && library && time_zone && weekday) !== '') {
+//       dispatch(getBookingTimes());
+//     }
+//   };
 
-export const updatingCheckBox = (language) => {
-  // console.log(languages)
-  return {
-    type: UPDATING_CHECKBOX,
-    payload: { name: 'language', value: language }
-  }
-}
+// const updateFormValues = (name, value) => {
+//   return { type: DROPDOWN_CHANGE, payload: { name, value } };
+// };
 
-/**
- * Update Booking form, then fetch booking time slots if all options are selected.
- */
-export const updatingBookingForm = (optionName, optionValue) => async (dispatch, getState) => {
-  dispatch(updateFormValues(optionName, optionValue));
-  const {language, library, time_zone, weekday} = getState().booking;
-  if ((language && library && time_zone && weekday) !=='' )  {
-    dispatch(getBookingTimes());
-  }
-}
+// /**
+//  * Gets the Times from the API and adds it to the bookings store
+//  * @returns void or error
+//  */
+// export const getBookingTimes = () => async (dispatch, getState) => {
+//   const authToken = getState().authToken;
+//   try {
+//     // get user from backend
+//     const headers = {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${authToken}`,
+//     };
 
-const updateFormValues = (name, value) => {
-  return {
-    type: DROPDOWN_CHANGE,
-    payload: { name, value }
-  }
-}
+//     // fetch fake data
+//     const getTimeResponse = fakeTimeData;
 
-/**
- * Gets the Times from the API and adds it to the bookings store
- * @returns void or error
- */
- export const getBookingTimes = () => async (dispatch, getState) => {
-  const authToken = getState().authToken;
-  try {
-    //get user from backend
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${authToken}`,
-    };
+//     dispatch(setTimeInBooking(getTimeResponse));
+//   } catch (err) {
+//     // manage what happens when things go wrong
+//     console.log('Error in getBookingTimes', err);
+//   }
+// };
 
-    // fetch fake data
-    const getTimeResponse = fakeTimeData
-
-    dispatch(setTimeInBooking(getTimeResponse));
-
-  } catch (err) {
-    //manage what happens when things go wrong
-    console.log('Error in getBookingTimes', err);
-  }
-};
-
-const setTimeInBooking = (time) => {
-  return {
-    type: SET_TIMES,
-    payload: time
-  }
-}
+// const setTimeInBooking = (time) => {
+//   return { type: SET_TIMES, payload: time };
+// };
 
 /**
  * Gets the Library and Language from the API and adds it to the bookings store
  * @returns void or error
  */
+// export const getBookingData = () => async (dispatch, getState) => {
+//   const authToken = getState().authToken;
+//   try {
+//     // get user from backend
+//     const headers = {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${authToken}`,
+//     };
 
-export const getBookingData = () => async (dispatch, getState) => {
+//     // fetch fake data
+//     const getLibraryResponse = fakeLibraryData;
+//     const getLanguageResponse = fakeLanguageData;
 
-  const authToken = getState().authToken;
-  try {
-    //get user from backend
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${authToken}`,
-    };
+//     dispatch( setLibraryAndLanguageInBooking(
+//         getLibraryResponse.data,
+//         getLanguageResponse.data
+//       )
+//     );
+//   } catch (err) {
+//     // manage what happens when things go wrong
+//     console.log('Error in getBookingData', err);
+//   }
+// };
 
-    // fetch fake data
-    const getLibraryResponse = fakeLibraryData
-    const getLanguageResponse = fakeLanguageData
-    
+// /**
+//  * Create post request to send data.
+//  * @returns void or error
+//  */
+// export const createBooking = () => async (dispatch, getState) => {
+//   const authToken = getState().authToken;
+//   try {
+//     // get user from backend
+//     const headers = {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${authToken}`,
+//     };
 
-    dispatch(setLibraryAndLanguageInBooking(getLibraryResponse.data, getLanguageResponse.data));
+//     // upload data to server-side
 
-  } catch (err) {
-    //manage what happens when things go wrong
-    console.log('Error in getBookingData', err);
-  }
-};
-
-
-const setLibraryAndLanguageInBooking = (library, language) => {
-  return {
-    type: SET_LIBRARIES_AND_LANGUAGES,
-    payload: {library, language}
-  }
-}
-
-/**
- * Create post request to send data.
- * @returns void or error
- */
- export const createBooking = () => async (dispatch, getState) => {
-  const authToken = getState().authToken;
-  try {
-    //get user from backend
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${authToken}`,
-    };
-
-    // upload data to server-side
-
-    alert('success!')
-
-  } catch (err) {
-        alert(
-          "We're sorry! Something went wrong while booking your appointment. Please contact your mentor advisor to find out more."
-        );
-        console.log(err);
-  }
-};
+//     alert('success!');
+//   } catch (err) {
+//     alert(
+//       "We're sorry! Something went wrong while booking your appointment. Please contact your mentor advisor to find out more."
+//     );
+//     console.log(err);
+//   }
+// };
--- src/redux/Booking.redux/Booking.reducer.js
+++ src/redux/Booking.redux/Booking.reducer.js
@@ -2,65 +2,71 @@ import moment from 'moment';
 import 'moment-timezone';
 
 import {
-    MENTOR_CHANGE,
-    COMMIT_CHANGE,
-    UPDATING_CHECKBOX,
-    DROPDOWN_CHANGE,
-    SET_LIBRARIES_AND_LANGUAGES,
-    SET_TIMES
-} from './Booking.action'
+  MENTOR_CHANGE,
+  COMMIT_CHANGE,
+  UPDATING_CHECKBOX,
+  DROPDOWN_CHANGE,
+  SET_LIBRARIES_AND_LANGUAGES,
+  SET_TIMES,
+  SET_BOOKING_LANGUAGE,
+} from './Booking.action';
 
 const BookingInitialState = {
-    libraries: [], 
-    languages: [], 
-    times: [], 
-    time_zone: moment.tz.guess(), 
-    language: '',
-    weekday: '', 
-    displayDay: '',
-    library: '', 
-    time: false,
-    displayTime: '',
-    isReturning: true, // 1
-    isCommitted: false,
-    sameAppointment: 'no'
+  libraries: [],
+  languages: [],
+  times: [],
+  time_zone: moment.tz.guess(),
+  language: '',
+  weekday: '',
+  displayDay: '',
+  library: '',
+  time: false,
+  displayTime: '',
+  isReturning: true, // 1
+  isCommitted: false,
+  sameAppointment: 'no',
 };
 
 export const booking = (state = BookingInitialState, action) => {
   //this needs to use the payload in someway
   switch (action.type) {
     case MENTOR_CHANGE:
-        let newLibrary = state.library
-        if ( !state.isReturning ) {
-            newLibrary = false
-        }
-        return {
-            ...state,
-            isReturning: !state.isReturning,
-            library: newLibrary
-        }
+      let newLibrary = state.library;
+      if (!state.isReturning) {
+        newLibrary = false;
+      }
+      return {
+        ...state,
+        isReturning: !state.isReturning,
+        library: newLibrary,
+      };
     case COMMIT_CHANGE:
-        return {
-            ...state,
-            isCommitted: !state.isCommitted
-        }
+      return {
+        ...state,
+        isCommitted: !state.isCommitted,
+      };
     case UPDATING_CHECKBOX:
     case DROPDOWN_CHANGE:
-        return {
-            ...state,
-            [action.payload.name]: action.payload.value
-        }
+      return {
+        ...state,
+        [action.payload.name]: action.payload.value,
+      };
     case SET_LIBRARIES_AND_LANGUAGES:
-        return {
-            ...state,
-            libraries: action.payload.library,
-            languages: action.payload.language
-        }
+      return {
+        ...state,
+        libraries: action.payload.library,
+        languages: action.payload.language,
+      };
     case SET_TIMES:
-        return {
-            ...state,
-            times: action.payload
-        }
+      return {
+        ...state,
+        times: action.payload,
+      };
+    case SET_BOOKING_LANGUAGE:
+      return {
+        ...state,
+        language: action.payload,
+      };
     default:
       return state;
   }

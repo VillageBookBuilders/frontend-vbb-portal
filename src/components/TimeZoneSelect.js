45721ce3ea2fb353df72cc22a92fefc6828b27da
Will Ley <wley3337@gmail.com>
2021-10-28 11:29:18 -0400

    Replace time zone select with generic time zone select

--- src/components/TimeZoneSelect.js
+++ src/components/TimeZoneSelect.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Select } from 'antd';
+
 import { timeZoneList } from '../util/timeZones';
 
 const timeZoneOptions = timeZoneList.map((tz) => ({ lable: tz, value: tz }));
@@ -8,8 +9,9 @@ const timeZoneOptions = timeZoneList.map((tz) => ({ lable: tz, value: tz }));
  * A searchable Timezone selection component.
  *
  * NOTE: Any changes to this component should be minimal.
- * If you need to add additional functionality give the arguments defaults and include them in the list below
- * @params onSelect handles when the user selects a time zone
+ * If you need to add additional functionality give the arguments defaults and
+ * include them in the list below
+ * @params handleSelect handles when the user selects a time zone. It is given the string representing the time zone selected i.e. 'America/New_York'
  * @params {obj of CSS styles } styles applied to the component
  * @returns Searchable Timezone selector Ant Design component
  */
@@ -31,26 +33,7 @@ export const SearchableTimeZoneSelection = ({ handleSelect, styles }) => {
       placeholder="Select a time zone"
       onSearch={onSearch}
       options={options}
-      onSelect={(e) => handleSelect(e)}
+      onSelect={handleSelect}
     />
   );
 };
-
-// Ant design Searchable Select
-// <Select
-// showSearch
-// style={{ width: 200 }}
-// placeholder="Select a person"
-// optionFilterProp="children"
-// onChange={onChange}
-// onFocus={onFocus}
-// onBlur={onBlur}
-// onSearch={onSearch}
-// filterOption={(input, option) =>
-//   option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-// }
-// >
-// <Option value="jack">Jack</Option>
-// <Option value="lucy">Lucy</Option>
-// <Option value="tom">Tom</Option>
-// </Select>
